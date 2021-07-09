88777f719d95984f24079bc41973039c83a63eb7
Abel <zxz054321@foxmail.com>
2021-07-09 17:35:24 +0800

    news analyser

--- analyser.py
+++ analyser.py
@@ -3,13 +3,20 @@ from collections import Counter
 import fire
 import jieba
 
-from orm import db, Article
+from orm import db, Article, News
 
 
 def policy_analyser():
     c = Counter()
     for article in Article.select():
         c.update(jieba.cut(article.content))
+    [output(word, count) for word, count in c.most_common()]
+
+
+def news_analyser():
+    c = Counter()
+    for news in News.select():
+        c.update(jieba.cut(news.content))
     [output(word, count) for word, count in c.most_common()]
 
 
@@ -23,5 +30,6 @@ if __name__ == '__main__':
     db.connect()
     fire.Fire({
         'policy': policy_analyser,
+        'news': news_analyser,
     })
     db.close()
